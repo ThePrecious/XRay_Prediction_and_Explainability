0b444b6ae79c29329ee6a05180381f9bd595ec9b
Arun Hegde <arun.hegde@berkeley.edu>
2022-10-09 22:19:27 -0700

    fixed path

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import os
 from src.app_gradio import make_frontend
 
-pth = os.getcwd()+'\\src\\gifsplanation'
+pth = os.getcwd()+'/src/gifsplanation'
 if not os.path.exists(pth):
     os.system('git clone https://github.com/mlmed/gifsplanation '+pth)
 
